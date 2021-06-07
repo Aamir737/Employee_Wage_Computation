4f4efc27a1b62d945ae425e7696ec24c698bfdbe
unknown <shaikh.ayan737@gmail.com>
2021-06-07 14:46:40 +0530

    Case Statement

--- EmpWage.sh
+++ EmpWage.sh
@@ -1,19 +1,23 @@
+echo "Welcome to Employee Wage Computation Program"
+
 isPartTime=1
 isFullTime=2
 empWagePerHour=20
 empHours=0
 checkemp=$((RANDOM%3))
-if [ $checkemp -eq $isPartTime ]
-then
-	echo "Employee Present Part Time"
-	empHours=8
-elif [ $checkemp -eq $isFullTime ]
-then
-	echo "Employee Present Full Time"
-	empHours=16
-else
-	echo "Employee is Absent"
-	empHours=0
-fi
+case $checkemp in
+	$isPartTime)
+		echo "Employee Present Part Time"
+		empHours=8
+		;;
+	$isFullTime)
+		echo "Employee Present Full Time"
+		empHours=16
+		;;
+	0)
+		echo "Employee is Absent"
+		empHours=0
+		;;
+esac
 totalWage=$(($empHours*$empWagePerHour))
 echo "The Daily Wage is : " $totalWage
